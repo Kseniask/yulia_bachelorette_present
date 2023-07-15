1097e233229f53bab5c575a19a20c1b56e030867
kseniask <kseniask30@icloud.com>
2023-07-15 09:28:02 -0700

    updated setences

--- src/content/answers.js
+++ src/content/answers.js
@@ -2,7 +2,7 @@ export const Answers = [
   {
     word: 'science world',
     question:
-      'You ride 12 stops from the nearest skytrain station to you home. Which building is mentioned in the name of station you ended up on?'
+      'You ride 12 stops from the nearest Skytrain station to your home. Which building is mentioned in the name of the station you ended up on?'
   },
   {
     word: 'main',
@@ -12,27 +12,27 @@ export const Answers = [
   {
     word: '1st',
     question:
-      'Yup, indeed! Ok, let`s go down the street. We are further from DT, yea, but you like walking, right? While walking you can see a place, it is literally read as fucking express. Really, these chinese people name places like that.. Ok, after this place turn right. Which street is it?'
+      "Yup, indeed! Ok, let's go down the street. We are further from DT, yea, but you like walking, right? While walking you can see a place, it is literally read as fucking express. Really, these chinese people name places like that.. Ok, after this place turn right. Which street is it?"
   },
   {
     word: 'false',
     question:
-      'Cool, you are walking on a first street. walk couple blocks away. You see the road splits and becomes boulevard? Good, you should turn left here. Is it Manitoba street that you turned to?'
+      'Cool, you are walking on a first street. Walk couple blocks away. You see the road splits and becomes boulevard? Good, you should turn left here. Is it Manitoba street that you turned to?'
   },
   {
     word: 'tacofino',
     question:
-      'Not the most beautiful street, but hey, we are going to have some dessert, it worth it. To the left side you can see the place service mexican food. it actually comes from Tofino, and their tacos are deliciousness. What is it called?'
+      'Not the most beautiful street, but hey, we are going to have some dessert, it worth it. To the left side you can see the place serving mexican food. It actually comes from Tofino, and their tacos are deliciousness. What is it called?'
   },
   {
     word: 'celery',
     question:
-      'Getting tired of HAVING FUN? Ok, I got you, It is the last one. Let`s turn right and go for one block. you can see the company right across the Manitoba street: The second word reminds of some vegetable. Which vegetable are we talking about?'
+      "Getting tired of HAVING FUN? Ok, I got you, it is the last one. Let's turn right and walk for one block. You can see the company right across the Manitoba street: The second word reminds of some vegetable. Which vegetable are we talking about?"
   },
   {
     word: 'penis',
     question:
-      'Lesson one: TRUST NOBODY! We LIED, it wasn`t the last one. This one is. Let`s walk a bit more. Go down Manitoba Street until you see a park. Cross it diagonally and follow Columbia street until you bump into East Broadway. ALMOST THERE! Turn right and find a place that has desserts. What is does dessert look like?'
+      "Lesson one: TRUST NOBODY! We LIED, it wasn't the last one. This one is. Let's walk a bit more. Go down Manitoba Street until you see a park. Cross it diagonally and follow Columbia Street until you bump into East Broadway. ALMOST THERE! Turn right and find a place that has desserts. What does dessert look like?"
   },
   {
     word: 'end',
