98e3d798d49ac0b758d1bb1c58ab690eecc83231
kseniask <kseniask30@icloud.com>
2023-07-15 02:53:12 -0700

    Added hunt implementation

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import WordGuessGame from './components/GameVisuals'
+
+function App () {
+  return <WordGuessGame />
+}
+
+export default App
---
+++ src/content/answers.js
@@ -0,0 +1,41 @@
+export const Answers = [
+  {
+    word: 'science world',
+    question:
+      'You ride 12 stops from the nearest skytrain station to you home. Which building is mentioned in the name of station you ended up on?'
+  },
+  {
+    word: 'main',
+    question:
+      'Good job. So you stepped out from the station. Do you see a huge balloon-thing? Science world, yea. Nah, you are not going there, we go the opposite side and bump into which street?'
+  },
+  {
+    word: '1st',
+    question:
+      'Yup, indeed! Ok, let`s go down the street. We are further from DT, yea, but you like walking, right? While walking you can see a place, it is literally read as fucking express. Really, these chinese people name places like that.. Ok, after this place turn right. Which street is it?'
+  },
+  {
+    word: 'false',
+    question:
+      'Cool, you are walking on a first street. walk couple blocks away. You see the road splits and becomes boulevard? Good, you should turn left here. Is it Manitoba street?'
+  },
+  {
+    word: 'tacofino',
+    question:
+      'Not the most beautiful street, but hey, we are going to have some dessert, it worth it. To the left side you can see the restoranto mexicano, but it actually comes from Tofino, it just serves mexical food. What is it called?'
+  },
+  {
+    word: 'celery',
+    question:
+      'Getting tired of HAVING FUN? Ok, I got you, I promise, it is the last one. Let`s turn right and go for one block. you can see the company right across the Manitoba street: The second word reminds of some vegetable. Which vegetable are we talking about?'
+  },
+  {
+    word: 'penis',
+    question:
+      'Lesson one: TRUST NOBODY! We LIED, it wasn`t the last one. This one is. Let`s walk a bit more. Go down Manitoba Street until you see a park. Cross it diagonally and follow Columbia street until you bump into Est Broadway. ALMOST THERE! Turn right and find a place that has desserts. What is does dessert look like?'
+  },
+  {
+    word: 'end',
+    question: 'Enjoy your penis! Hehe'
+  }
+]
